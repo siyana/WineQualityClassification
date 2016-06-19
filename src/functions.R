b7e1ebf5dc774ac7f4225264bdca409cfecda033
siyana <siyanaslavova@gmail.com>
2016-06-19 17:46:58 +0300

    generate test and train sets

---
+++ src/functions.R
@@ -0,0 +1,8 @@
+splitSet <- function (set, testPath, trainPath) {
+    indexes <- sample(2, nrow(set), replace=TRUE, prob=c(0.7, 0.3)) #create a sample list ot 1/2
+    trainData <- set[indexes == 1,] # indexes == 1 returns true or false for each value
+    testData <- set[indexes == 2,] 
+	
+    write.csv(trainData , file = trainPath , row.names=FALSE, fileEncoding = "UTF-8") 
+    write.csv(testData , file = testPath , row.names=FALSE, fileEncoding = "UTF-8") 
+}
---
+++ src/generateSets.R
@@ -0,0 +1,16 @@
+basePath = "E:\\Projects\\WineQuality\\WineQualityClassification\\"
+source(paste(basePath, "\\src\\functions.R", sep=""))
+
+redWinePath = paste(basePath,"\\resources\\winequality-red.csv",sep = "")
+redWineTrainPath = paste(basePath,"\\resources\\redWineTrain.csv",sep = "")
+redWineTestPath = paste(basePath,"\\resources\\redWineTest.csv",sep = "")
+
+redWineSet = read.csv(redWinePath,sep=";")
+splitSet(redWineSet, redWineTestPath, redWineTrainPath)
+
+whiteWineTrainPath = paste(basePath,"\\resources\\whiteWineTrain.csv",sep = "")
+whiteWineTestPath = paste(basePath,"\\resources\\whiteWineTest.csv",sep = "")
+whiteWinePath = paste(basePath,"\\resources\\winequality-white.csv",sep = "")
+
+whiteWineSet = read.csv(whiteWinePath, sep=";")
+splitSet(whiteWineSet, whiteWineTestPath, whiteWineTrainPath)
