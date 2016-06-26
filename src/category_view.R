f4d35fd6b0dd77cc33df274af7f76fc9821d4669
skeleta <ivankapukaranov@gmail.com>
2016-06-26 14:06:08 +0300

    Add wines quality distribution plots.

---
+++ src/category_view.R
@@ -0,0 +1,34 @@
+source(paste(getwd(),"\\src\\global_constants.R", sep = ""))
+source(paste(basePath, "\\src\\functions.R", sep=""))
+
+#WhiteWineTrain
+whiteWineTrain = read.csv(whiteWineTrainPath)
+
+attach(whiteWineTrain)
+png(filename = boxplotFileName("whiteWine", "test_quality_distribution"))
+   barplot(table(whiteWineTrain$quality))
+dev.off()
+
+#WhiteWineTest
+whiteWineTest = read.csv(whiteWineTestPath)
+
+attach(whiteWineTest)
+png(filename = boxplotFileName("whiteWine", "test_quality_distribution"))
+   barplot(table(whiteWineTest$quality))
+dev.off()
+
+#RedWineTrain
+redWineTrain = read.csv(redWineTrainPath)
+
+attach(redWineTrain)
+png(filename = boxplotFileName("redWine", "train_quality_distribution"))
+   barplot(table(redWineTrain$quality))
+dev.off()
+
+#RedWineTest
+redWineTest = read.csv(redWineTestPath)
+
+attach(redWineTest)
+png(filename = boxplotFileName("redWine", "test_quality_distribution"))
+   barplot(table(redWineTest$quality))
+dev.off()
