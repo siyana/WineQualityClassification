d89167270382e5aad1ad7b3a1e56ed7d8afff069
skeleta <ivankapukaranov@gmail.com>
2016-06-28 23:35:25 +0300

    Revert the chages in src/neural_network_mixedWine.R

--- src/neural_network_mixedWine.R
+++ src/neural_network_mixedWine.R
@@ -6,27 +6,24 @@ source(paste(getwd(),"\\src\\global_constants.R", sep = ""))
 source(paste(basePath, "\\src\\functions.R", sep=""))
 
 #mixedWineTrain
-dropColumnNames = c("chlorides", "residual.sugar", "fixed.acidity", "free.sulfur.dioxide")
 mixedWineTrain = read.csv(mixedWineTrainPath)
 
 attach(mixedWineTrain)
-columnsTrain = mixedWineTrain[,!names(mixedWineTrain) %in% dropColumnNames]
+
 #WhiteWineTest
 whiteWineTest = read.csv(whiteWineTestPath)
 attach(whiteWineTest)
 
-#inputTrain = mixedWineTrain[,1:11];
-inputTrain = columnsTrain
-outputTrain = decodeClassLabels(columnsTrain$quality)
+inputTrain = mixedWineTrain[,1:11];
+outputTrain = decodeClassLabels(mixedWineTrain$quality)
 trainData <- splitForTrainingAndTest(inputTrain , outputTrain , ratio = 0)
 trainData <- normTrainingAndTestSet(trainData)
 
 maxis = 1000
 size = 100
 
-columnsTest = whiteWineTest[,!names(whiteWineTest) %in% dropColumnNames]
-inputTest = columnsTest
-outputTest = decodeClassLabels(columnsTest$quality)
+inputTest = whiteWineTest[,1:11]
+outputTest = decodeClassLabels(whiteWineTest$quality)
 
 mixedWineTrainResultsPath = paste(basePath,"//results//NN//mixed_whiteWine_matrix_train.csv", sep = "")
 whiteTestResultsPath =  paste(basePath,"//results//NN//mixed_whiteWine_matrix_test.csv", sep = "")
@@ -68,6 +65,6 @@ write(paste("-----\n", "recall: ", maxSumComponents[2],"accuracy;",maxSumCompone
 empty_col = c(0,0,0,0,0,0,0)
 matrix = cbind(empty_col, empty_col,maxModelMatrix,empty_col, empty_col, empty_col)
 
-#write.table(paste("-----\n", "f1: ", f1(matrix)), whiteTestResultsPath, append = TRUE)
-#write.table(paste("precision: ", precision(matrix)), whiteTestResultsPath, append = TRUE)
-#write.table(paste("recall: ", recall(matrix)), whiteTestResultsPath, append = TRUE)
+write.table(paste("-----\n", "f1: ", f1(matrix)), whiteTestResultsPath, append = TRUE)
+write.table(paste("precision: ", precision(matrix)), whiteTestResultsPath, append = TRUE)
+write.table(paste("recall: ", recall(matrix)), whiteTestResultsPath, append = TRUE)
